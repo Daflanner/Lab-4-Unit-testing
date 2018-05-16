a4554297e25cea520403d0471843f33ff9ca2160
Daniel Flannery <mr.dan.flannery@gmail.com>
2018-05-16 15:19:47 -0700

    completed cups to tablespoon conversion tests

--- src/edu/pcc/cis/conversion/ConversionFunctions.java
+++ src/edu/pcc/cis/conversion/ConversionFunctions.java
@@ -27,7 +27,9 @@ public class ConversionFunctions {
     public static int cupsToTablespoons(int cups) {
         // This method does not work correctly. You will fix it as you work through the tests.
         // Remove these comments once it is implemented correctly.
-        return 0;
+            if(cups <0 ){ return 0;}
+        return (int) cups * 16;
+
     }
 
     /**
--- src/edu/pcc/cis/conversion/ConversionFunctionsTest.java
+++ src/edu/pcc/cis/conversion/ConversionFunctionsTest.java
@@ -33,12 +33,31 @@ public class ConversionFunctionsTest {
 
     // cupsToTablespoons tests ---------------------------------------------------------------------------
     // TODO: Write a test for 0 cups converts to 0 tablespoons - run the test & see that it passes
+    @Test
+    public void cupsToTablespoons0() {assertEquals( 0, ConversionFunctions.cupsToTablespoons(0)); }
+
 
     // TODO: Write a test for 1 cup converts to 16 tablespoons
+    @Test
+    public void cups1ToTablespoons16() {assertEquals( 16, ConversionFunctions.cupsToTablespoons(1)); }
+
+
     // TODO: Run the test & see that it fails. Failing tests reveal bugs.
+    @Test
+    public void neg3cupsToTablespoons() {assertEquals( 47, ConversionFunctions.cupsToTablespoons(3)); }
+
+
     // TODO: Fix the implementation of cupsToTablespoons so that this test passes
+    @Test
+    public void fix3cupsToTablespoons() {assertEquals( 48, ConversionFunctions.cupsToTablespoons(3)); }
+
 
     // TODO: Write one or two additional tests for cupsToTablespoons
+    @Test
+    public void bigCupsToBigTablespoons() {assertEquals( 1600000000, ConversionFunctions.cupsToTablespoons(100000000)); }
+
+    @Test
+    public void NegCupsToNegTablespoons() {assertEquals( 0, ConversionFunctions.cupsToTablespoons(-2)); }
 
 
     // monthNumToMonthName tests ---------------------------------------------------------------------------
